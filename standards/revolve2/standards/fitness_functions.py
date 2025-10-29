951f3cd12d6665dc297bd97018304d6cc3252c40
Vebjørn Bye Karlsen <vebjorbk@uio.no>
2025-10-29 20:03:02 +0100

    y fitness

--- standards/revolve2/standards/fitness_functions.py
+++ standards/revolve2/standards/fitness_functions.py
@@ -34,5 +34,18 @@ def x_displacement(
     """
     begin_position = begin_state.get_pose().position
     end_position = end_state.get_pose().position
-    return  end_position.x - begin_position.x
+    return  abs(end_position.x - begin_position.x)
 
+def y_displacement(
+    begin_state: ModularRobotSimulationState, end_state: ModularRobotSimulationState
+) -> float:
+    """
+    Calculate the distance traveled on the y-axis by a single modular robot.
+
+    :param begin_state: Begin state of the robot.
+    :param end_state: End state of the robot.
+    :returns: The calculated fitness.
+    """
+    begin_position = begin_state.get_pose().position
+    end_position = end_state.get_pose().position
+    return  abs(end_position.y - begin_position.y)
